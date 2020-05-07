b1bd04104e78f75e84ec962e938660ea9a85a8e8
Maxime <valliemaxime@gmail.com>
2020-05-07 17:42:50 +0200

    Appli régler et ok pour Heroku

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,16 @@
 class ArticlesController < ApplicationController
-
+before_action :set_article, only: [:edit, :update, :show, :destroy]
 	def new
     @article = Article.new
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
+
+	def index
+		@articles = Article.all
+	end
 
   def create
     @article = Article.new(article_params)
@@ -20,9 +24,8 @@ class ArticlesController < ApplicationController
   end
 
 def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = “artcile bien modifié”
+      flash[:notice] = "artcile bien modifié"
   	  redirect_to articles_path(@article)
     else
   	  render "edit"
@@ -30,14 +33,20 @@ def update
 end
 
 
-
-
-
   def show
-    @article = Article.find(params[:id])
   end
 
+	def destroy
+     @article.destroy
+		 flash[:notice] = "Article supprimé"
+     redirect_to articles_path
+	 end
+
   private
+
+	def set_article
+      @article = Article.find(params[:id])
+	end
 
   def article_params
   	params.require(:article).permit(:title, :description)
