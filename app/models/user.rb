16c176fa3fa516eaac412fb36cc77e98e80eecfa
Maxime <valliemaxime@gmail.com>
2020-05-10 00:43:29 +0200

    add password

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     uniqueness: {case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
-
+  has_secure_password
 end
