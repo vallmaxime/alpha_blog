45315523734884cf53fc7bbb89f93a985d278518
Maxime <valliemaxime@gmail.com>
2020-05-09 21:41:42 +0200

    add association beetwin user ans articles and some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :articles
+
+  before_save { self.email.downcase! }
   validates :username, presence: true,
                        uniqueness: {case_sensitive: false },
                        length: {minimum: 3, maximum: 25}
@@ -7,6 +10,6 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true,
                     uniqueness: {case_sensitive: false },
-                    format: { with : VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX }
 
 end
---
+++ db/migrate/20200509152400_user_id_to_articles.rb
@@ -0,0 +1,5 @@
+class UserIdToArticles < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :user_id, :integer
+  end
+end
