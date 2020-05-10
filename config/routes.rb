0046c89d2bc86588408da29113fbf0ff0391d913
Maxime <valliemaxime@gmail.com>
2020-05-10 19:51:26 +0200

    Creat user from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :articles
+
   root 'pages#home'
   get 'about' , to: 'pages#about'
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
 end
