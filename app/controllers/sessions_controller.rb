2c7bae32b3765a85c348d3857ad5dbfc43dff41d
Maxime <valliemaxime@gmail.com>
2020-05-11 19:23:18 +0200

    create & destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render "new"
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Felicitation pour cette incroyable connexion !"
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Identifiants incorrects borww"
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:success] = "Aurevoir maggle"
+    redirect_to root_path
   end
 
 end
