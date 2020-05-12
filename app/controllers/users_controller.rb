afa7b3f70b4aaffd32f6c561f1cc824b8ff39007
Maxime <valliemaxime@gmail.com>
2020-05-13 00:32:22 +0200

    modification du file user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,77 +1,70 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :updates, :show, :destroy]
-  before_action :require_save_user, only: [:edit, :update, :destroy]
-  before_action :require_admin, only: [:destroy]
+    before_action :set_user, only: [:edit, :update, :show, :destroy]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
 
-  def index
-    @users = User.paginate(:page => params[:page], :per_page => 5)
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      flash[:success] = "New user #{@user.username} created!"
-      redirect_to user_path(@user)
-    else
-      render 'new'
+    def index
+        @users = User.paginate(:page => params[:page], :per_page => 5)
     end
-  end
 
-  def edit
-  end
-
-  def update
-    if @user.update(user_params)
-      flash[:success] = "User mis à jour avec success !"
-      redirect_to articles_path
-    else
-      render 'edit'
+    def new
+        @user = User.new
     end
-  end
 
-  def show
-    @user_articles = @user.articles.paginate(:page => params[:page], :per_page => 5)
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def destroy
-    @user.destroy
-    flash[:success] = "Utilisateur et touts ces articles supprimé avec succès"
-    redirect_to users_path
-  end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
-
-  def require_save_user
-    if current_user != @user and current_user.admin? == false
-      flash[:danger] = "Hop hop hop !! non non non mon con =)"
-      redirect_to root_path
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            flash[:success] = "New user #{@user.username} created !"
+            redirect_to user_path(@user)
+        else
+            render 'new'
+        end
     end
-  end
 
-  def require_admin
-    if logged_in? and current_user.admin? == false
-      flash[:danger] = "Action impossible si tu n'es pas admin"
-      redirect_to users_path
+    def edit
     end
-  end
 
+    def update
+        if @user.update(user_params)
+            flash[:success] = "User mis a jour avec succes !"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
 
+    def show
+        @user_articles = @user.articles.paginate(:page => params[:page], :per_page => 5)
+    end
 
+    def destroy
+        @user.destroy
+        flash[:success] = "Utilisateur et tous ses articles supprimés avec succès mon pote"
+        redirect_to users_path
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+        if current_user != @user and current_user.admin? == false
+           flash[:danger] = "Ooh la garçon ! Tu te crois ou la !?"
+           redirect_to root_path
+        end
+    end
+
+    def require_admin
+        if logged_in? and current_user.admin? == false
+           flash[:danger] = "Action impossible si t'es pas admin ma gueule"
+           redirect_to users_path
+        end
+    end
 end
